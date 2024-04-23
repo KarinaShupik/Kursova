139c2308fede49faa0cb9a99ad2b1e5adb583a1d
Karina Shupik <77788896h@gmail.com>
2024-04-23 08:22:04 +0300

    feat: add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Home} from "./pages/Home"
+import {SignIn} from "./pages/SignIn"
+import {SignUp} from "./pages/SignUp"
+import {Profile} from "./pages/Profile"
 
 function App() {
-  
   return (
-    <></>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 

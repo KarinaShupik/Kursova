c9988918ebc8db33db93ff2b31807fdd2068714f
Karina Shupik <77788896h@gmail.com>
2024-05-09 08:47:27 +0300

    feat: add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
+
+//middeware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;//if error happens on server side wejust put here 500 server error
+    const message = err.message || "Internal Server Error"//if there is no massage we just show our message
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})

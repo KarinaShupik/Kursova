715cb186c726aee03e3439ddec48e5574a83b3f0
Karina Shupik <77788896h@gmail.com>
2024-05-17 16:08:33 +0300

    feat: add sign-up style

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,24 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import '../styles/SignUp.css'
 
 export function SignUp() {
-  
     return (
-      <h1>SignUp</h1>
+      <div className="sign-up_container">
+        <h1 className="page_title">Sign Up</h1>
+        <form className="sign-up_form">
+          <input type="text" placeholder="Username" id="username" className="username_input"/>
+          <input type="email" placeholder="Email" id="email" className="email_input"/>
+          <input type="password" placeholder="Password" id="password" className="password_input"/>
+          <button className="sign-up_btn">Sign Up</button>
+        </form>
+        <div className="add_info_sign-up">
+          <p>Have an account</p>
+          <Link to="/sing-in">
+            <span className="sign-in_link">Sign In</span>
+          </Link>
+        </div>
+      </div>
     )
   }
   

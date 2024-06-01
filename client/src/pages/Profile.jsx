e4d71496ae4bcbde3e2f39875e3f3eedf8146d10
Karina Shupik <77788896h@gmail.com>
2024-06-01 10:45:01 +0300

    feat: complete profile page functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import { useSelector } from "react-redux"
 
 export function Profile() {
+    const {currentUser} = useSelector(state => state.user)
     return (
-        <><h1>Profile</h1></>
+        <div className="profile_stage">
+            <h1 className="profile_title">Profile</h1>
+            <form className="profile_form">
+                <img src={currentUser.profilePicture} alt="Profile" className="profile_photo"/>
+                <input defaultValue={currentUser.username} type="text" id="username" placeholder="Username" className="username_pf_input"/>
+                <input defaultValue={currentUser.email} type="email" id="email" placeholder="Email" className="email_pf_input"/>
+                <input type="password" id="password" placeholder="Password" className="password_pf_input"/>
+                <button className="profile_btn">Update</button>
+            </form>
+        <div className="additional_btn">
+            <span className="delete_btn">Delete Account</span>
+            <span className="signout_btn">Sign Out</span>
+        </div>
+        </div>
     )
   }

65ba1a7185ce6769153a7639750b7218eb4a867b
Karina Shupik <77788896h@gmail.com>
2024-06-02 17:52:32 +0300

    feat: gallery

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Hero from "../pages/Hero.jsx"
 import Gallery from "../pages/Gallery.jsx"
+import News from "../pages/News.jsx"
 
 export function Home() {
     return (
       <>
         <Hero />
         <Gallery />
+        <News />
       </>
     )
 }
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,34 +1,36 @@
-import React from "react"
+import img1 from "../img/image 8.png"
+import img2 from "../img/image 9.png"
+import img3 from "../img/image 10.png"
+import img4 from "../img/image 11.png"
+import img5 from "../img/image 12.png"
+import img6 from "../img/image 13.png"
+import img7 from "../img/image 14.png"
+import "../styles/Gallery.css"
 
 export default function Gallery() {
     return (
         <div className="gallery-container">
         <h1>Gallery</h1>
         <div className="gallery">
-            <div className="gallery-item">
-                <img src="img/image1.jpg" alt="Image 1" />
+            <div className="gallery-item div1">
+                <img src={img1} alt="Image 1" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image2.jpg" alt="Image 2" />
+            <div className="gallery-item div2">
+                <img src={img2} alt="Image 2" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image3.jpg" alt="Image 3" />
+            <div className="gallery-item div3">
+                <img src={img3} alt="Image 3" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image4.jpg" alt="Image 4" />
+            <div className="gallery-item div4">
+                <img src={img4} alt="Image 4" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image5.jpg" alt="Image 5" />
+            <div className="gallery-item div5">
+                <img src={img5} alt="Image 5" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image6.jpg" alt="Image 6" />
+            <div className="gallery-item div6">
+                <img src={img6} alt="Image 6" />
             </div>
-            <div className="gallery-item">
-                <img src="img/image7.jpg" alt="Image 7" />
-            </div>
-            <div className="gallery-item">
-                <img src="img/image8.jpg" alt="Image 8" />
-            </div>
+            
         </div>
     </div>
     )

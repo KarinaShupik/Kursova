96188af702e17a9ebbeebf0efcaaa49648fd614b
Karina Shupik <77788896h@gmail.com>
2024-06-10 18:08:33 +0300

    fix: deploy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
-import path from "path"
+//import path from "path"
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,13 +17,13 @@ const app = express()
 
 app.use(express.json())
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+///*app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });
+  });*/
 
 app.use(cookieParser())
 
